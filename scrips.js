2e716b4b8867b85d310f2b52144ed118cf1f9b41
nowitches <79973750+nowitches@users.noreply.github.com>
2023-03-27 12:15:33 +0300

    Changes to tiles

--- scrips.js
+++ scrips.js
@@ -3,7 +3,7 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 function getKristinasTitle()
 {
-    var titleArray= new Array("disabled person","senior UX designer", "person", "illustrator", "video game enthusiast", "design systems expert", "design generalist");
+    var titleArray= new Array("disabled person","senior UX designer", "design mentor", "digital illustrator", "video game enthusiast", "design systems expert", "design generalist");
     var random = titleArray[Math.floor(Math.random() * titleArray.length)];
    document.getElementById("res").innerHTML=random;
 }
