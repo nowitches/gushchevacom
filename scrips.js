aff350360201e9b2376bfaeb12fd2bfcc71df929
Kristina <christina.gushcheva@gmail.com>
2024-04-17 19:11:03 +0300

    Portfolio changes

--- scrips.js
+++ scrips.js
@@ -3,7 +3,7 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 function getKristinasTitle()
 {
-    var titleArray= new Array("Kristina Gushcheva-Keippilä is a design leader","Kristina Gushcheva-Keippilä is a product designer", "Kristina Gushcheva-Keippilä is an experienced design mentor", "Kristina Gushcheva-Keippilä is a digital illustrator", "Kristina Gushcheva-Keippilä is a video game enthusiast", "Kristina Gushcheva-Keippilä is a writer", "Kristina Gushcheva-Keippilä is a design generalist", "Kristina Gushcheva-Keippilä is an accessibility expert", "Kristina Gushcheva-Keippilä is a trivia collector", "Kristina Gushcheva-Keippilä is a systems thinking enthusiast", "Kristina Gushcheva-Keippilä is a disabled person");
+    var titleArray= new Array("Kristina Gushcheva-Keippilä is a product design leader","Kristina Gushcheva-Keippilä is a product designer", "Kristina Gushcheva-Keippilä is a design generalist", "Kristina Gushcheva-Keippilä is an accessibility expert", "Kristina Gushcheva-Keippilä is a trivia collector", "Kristina Gushcheva-Keippilä thinks in systems", "Kristina Gushcheva-Keippilä is a disabled person");
     var random = titleArray[Math.floor(Math.random() * titleArray.length)];
    document.getElementById("res").innerHTML=random;
 }
